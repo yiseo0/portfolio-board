fd6e9340c0b90b1528e5bc1a28d2ced59693dfc0
flfk55 <flfk55@naver.com>
2023-05-08 04:09:32 +0900

    feat : Graphql-API로 게시물 등록 기능 구현

--- pages/boards/new/index.js
+++ pages/boards/new/index.js
@@ -1,10 +1,32 @@
 import { Form, Button, WriteBox, Title, Wrapper, Write, WriteContent, WriteAddress, Label, Upload, RadioLabel, Radio, ButtonSubmit, Error } from "@/styles/emotion";
+import { gql, useMutation } from "@apollo/client";
 import { useForm } from "react-hook-form";
+
+const CREATE_BOARD = gql`
+   mutation createBoard($createBoardInput : CreateBoardInput!){
+      createBoard(createBoardInput: $createBoardInput){
+         _id
+      }
+   }
+`
 
 export default function BoardNew() {
    const { register, handleSubmit, formState: { errors } } = useForm()
-   const onSubmit = data => {
-      // console.log(data)
+   const [createBoard] = useMutation(CREATE_BOARD)
+
+   const onSubmit = async data => {
+      const { writer, password, title, contents } = data
+      const result = await createBoard({
+         variables: {
+            createBoardInput: {
+               writer,
+               password,
+               title,
+               contents,
+            }
+         }
+      })
+      console.log(result)
    }
 
    return (
@@ -13,8 +35,8 @@ export default function BoardNew() {
          <Form method="POST" onSubmit={handleSubmit(onSubmit)}>
             <WriteBox row="2">
                <Label htmlFor="">작성자</Label>
-               <Write type="text" placeholder="이름을 입력해주세요." {...register("name", { required: "이름을 입력해주세요." })} />
-               <Error>{errors.name?.message}</Error>
+               <Write type="text" {...register("writer", { required: "이름을 입력해주세요." })} />
+               <Error>{errors.writer?.message}</Error>
             </WriteBox>
             <WriteBox row="2">
                <Label htmlFor="">비밀번호</Label>
@@ -28,19 +50,19 @@ export default function BoardNew() {
             </WriteBox>
             <WriteBox>
                <Label htmlFor="">내용</Label>
-               <WriteContent type="text" placeholder="내용을 작성해주세요." {...register("content", { required: "내용을 작성해주세요." })} />
-               <Error>{errors.content?.message}</Error>
+               <WriteContent type="text" placeholder="내용을 작성해주세요." {...register("contents", { required: "내용을 작성해주세요." })} />
+               <Error>{errors.contents?.message}</Error>
             </WriteBox>
             <WriteBox>
                <Label htmlFor="">주소</Label>
-               <WriteAddress type="text" placeholder="07250" />
+               <WriteAddress type="text" placeholder="07250" {...register("zipCode")} />
                <Button color="white" type="button" value="우편번호 검색" />
-               <Write type="text" />
-               <Write type="text" />
+               <Write type="text" {...register("address")} />
+               <Write type="text" {...register("addressDetail")} />
             </WriteBox>
             <WriteBox>
                <Label htmlFor="">유튜브</Label>
-               <Write type="text" placeholder="링크를 복사해주세요." />
+               <Write type="text" placeholder="링크를 복사해주세요." {...register("youtube")} />
             </WriteBox>
             <WriteBox>
                <Label htmlFor="">사진 첨부</Label>
@@ -51,12 +73,12 @@ export default function BoardNew() {
             <WriteBox>
                <Label htmlFor="">메인 설정</Label>
                <RadioLabel htmlFor="youtube">
-                  <Radio type="radio" name="mainChk" id="youtube" />
+                  <Radio type="radio" id="youtube" {...register("mainChk")} />
                   <i></i>
                   유튜브
                </RadioLabel>
                <RadioLabel htmlFor="picture">
-                  <Radio type="radio" name="mainChk" id="picture" />
+                  <Radio type="radio" id="picture" {...register("mainChk")} />
                   <i></i>
                   사진
                </RadioLabel>
@@ -65,4 +87,4 @@ export default function BoardNew() {
          </Form>
       </Wrapper>
    );
-} ``
+}
