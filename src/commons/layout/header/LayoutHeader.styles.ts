ed9b8c3af584700083220917e8c0ca282bcc46f4
정혜진 <flfk55@naver.com>
2023-06-04 23:38:31 +0900

    feat : 레이아웃 네비게이션 구현

--- src/commons/layout/header/LayoutHeader.styles.ts
+++ src/commons/layout/header/LayoutHeader.styles.ts
@@ -2,7 +2,7 @@ import styled from "@emotion/styled";
 
 export const Wrapper = styled.div``;
 export const Inner = styled.div`
-  width: 1200px;
+  max-width: 1200px;
   margin: 0 auto;
   height: 150px;
   display: flex;
--- src/commons/layout/index.tsx
+++ src/commons/layout/index.tsx
@@ -1,5 +1,6 @@
 import styled from "@emotion/styled";
 import LayoutHeader from "./header/LayoutHeader.container";
+import LayoutNavigation from "./navigation/LayoutNavigation.container";
 
 interface ILayoutProps {
    children: JSX.Element;
@@ -14,7 +15,7 @@ export default function Layout(props: ILayoutProps) {
       <>
          <LayoutHeader />
          {/* <LayoutBanner /> */}
-         {/* <LayoutNavigation /> */}
+         <LayoutNavigation />
          <Body>{props.children}</Body>
       </>
    );
